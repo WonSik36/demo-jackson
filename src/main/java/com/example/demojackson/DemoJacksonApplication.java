7636412d9fdafc6c9902507d1728d54bb8506325
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-10-19 08:24:09 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/demojackson/DemoJacksonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJacksonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJacksonApplication.class, args);
+    }
+
+}
