8bd4083f5b203f832f04ce4c73a507c0a5708c81
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-10-19 21:19:11 +0900

    테스트 4개 추가
    - @JsonGetter
    - @JsonPropertyOrder
    - @JsonValue

--- src/test/java/com/example/demojackson/ex/Ex1.java
+++ src/test/java/com/example/demojackson/ex/Ex1.java
@@ -2,8 +2,11 @@ package com.example.demojackson.ex;
 
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 
@@ -34,7 +37,6 @@ public class Ex1 {
     }
 
     public static class JsonAnyGetterObject {
-        @Setter
         public String name;
         private final Map<String, String> properties = new HashMap<>();
 
@@ -45,6 +47,67 @@ public class Ex1 {
         @JsonAnyGetter
         public Map<String, String> getProperties() {
             return properties;
+        }
+    }
+
+    @Test
+    void whenSerializingUsingJsonGetter_thenCorrect() throws Exception {
+        JsonGetterObject o = new JsonGetterObject();
+        o.firstName = "firstName";
+        o.lastName = "lastName";
+
+        String result = objectMapper.writeValueAsString(o);
+
+        log.info(result);
+        assertThat(result).isEqualTo("{\"firstName\":\"firstName\",\"lastName\":\"lastName\"}");
+    }
+
+    public static class JsonGetterObject {
+        public String firstName;
+        private String lastName;
+
+        @JsonGetter("lastName")
+        public String getLastName() {
+            return lastName;
+        }
+    }
+
+    @Test
+    void whenSerializingUsingJsonPropertyOrder_thenCorrect() throws Exception {
+        JsonPropertyOrderObject o = new JsonPropertyOrderObject();
+        o.firstName = "firstName";
+        o.lastName = "lastName";
+
+        String result = objectMapper.writeValueAsString(o);
+
+        log.info(result);
+        assertThat(result).isEqualTo("{\"lastName\":\"lastName\",\"firstName\":\"firstName\"}");
+    }
+
+    @JsonPropertyOrder({"lastName","firstName"})
+    public static class JsonPropertyOrderObject {
+        public String firstName;
+        public String lastName;
+    }
+
+    @Test
+    void whenSerializingUsingJsonValue_thenCorrect() throws Exception {
+        String result = objectMapper.writeValueAsString(TypeEnumWithValue.TYPE1);
+
+        log.info(result);
+        assertThat(result).isEqualTo("\"Type A\"");
+    }
+
+    @AllArgsConstructor
+    public enum TypeEnumWithValue {
+        TYPE1(1, "Type A"), TYPE2(2, "Type 2");
+
+        private Integer id;
+        private String name;
+
+        @JsonValue
+        public String getName() {
+            return name;
         }
     }
 }
